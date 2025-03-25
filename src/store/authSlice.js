17dc3efc8e803b1c50ba418dc25e85645a606eeb
hosurVijay <hosurvijayendra@gmail.com>
2025-03-25 22:22:12 +0530

    Save local changes before pulling

---
+++ src/store/authSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit"
+import { act } from "react"
+
+const initialState = {
+    status : false,
+    userData : null
+}
+
+const authSlice = createSlice({
+    name : "auth",
+    initialState,
+    reducers : {
+        login : (state, action) => {
+            state.status = true;
+            state.userData = action.payload.userData;
+        },
+        logout : (state, action) => {
+            state.state = false;
+            state.userData = null;
+        }
+
+    }
+})
+
+export const {login, logout} = authSlice.actions
+
+export default authSlice.reducer
